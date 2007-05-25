2420c457c907500acef9375b161326feedd484b6
John V. Sichi <jsichi@gmail.com>
2007-05-24 20:02:17 -0800

    INTEGRATION: Integration of 9274,9286,9293,9312 from parent to child (branch spec: dev_to_lu_dev).    ==> 9274, Date:	2007/05/16 23:03:25        User:	jhyde        Description:        	DEV: integrate from //open/dt/dev@9269.
       ==> 9286, Date:	2007/05/19 00:03:00
           User:	jvs
           Description:
           	FARRAGO:  (FRG-271) re-enable shutdown assert in FarragoObjectCache
    
       ==> 9293, Date:	2007/05/21 11:35:46
           User:	jvs
           Description:
           	DEV/LUCIDDB:  update log for fix from DT in timestamp rounding to
           	precision 0
    
       ==> 9312, Date:	2007/05/23 22:56:29
           User:	jvs
           Description:
           	DEV: cherry-pick integrate eigenchange 9296 (log4j fix
           	       to avoid the need to run ant during thirdparty make) from
           	       //open/dt/dev
    
    [git-p4: depot-paths = "//open/lu/dev/": change = 9323]

--- farrago/src/net/sf/farrago/type/FarragoTypeFactoryImpl.java
+++ farrago/src/net/sf/farrago/type/FarragoTypeFactoryImpl.java
@@ -829,6 +829,21 @@ public class FarragoTypeFactoryImpl
                 EncodedSqlInterval.GET_START_UNIT_METHOD_NAME,
                 lookupTimeUnit(qualifier.getStartUnit())));
 
+        if (qualifier.getEndUnit() != null) {
+            memberDecls.add(
+                generateGetter(
+                    timeUnitType,
+                    EncodedSqlInterval.GET_END_UNIT_METHOD_NAME,
+                    lookupTimeUnit(qualifier.getEndUnit())));
+        } else {
+            memberDecls.add(
+                generateGetter(
+                    timeUnitType,
+                    EncodedSqlInterval.GET_END_UNIT_METHOD_NAME,
+                    Literal.constantNull()));
+        }
+
+
         return
             newHolderOJClass(
                 superclass,
@@ -837,6 +852,9 @@ public class FarragoTypeFactoryImpl
                 type);
     }
 
+    /**
+     * Generate an expression to return 
+     */
     /**
      * Generates an expression for a TimeUnit
      */
--- farrago/src/net/sf/farrago/util/FarragoObjectCache.java
+++ farrago/src/net/sf/farrago/util/FarragoObjectCache.java
@@ -63,7 +63,7 @@ public class FarragoObjectCache
      * synchronized via mapKeyToEntry monitor.
      */
     private long bytesUsed;
-    
+
     /**
      * Victimization policy for this cache
      */
@@ -408,8 +408,9 @@ public class FarragoObjectCache
                     + ", size " + entry.memoryUsage);
             }
 
-            assert (entry.pinCount == 0) : entry.pinCount;
-            assert (entry.constructionThread == null);
+            assert (entry.pinCount == 0) :
+                "cache entry " + entry + " has pin count " + entry.pinCount;
+            assert (entry.constructionThread == null) : entry;
 
             if (entry.value instanceof FarragoAllocation) {
                 ((FarragoAllocation) (entry.value)).closeAllocation();
